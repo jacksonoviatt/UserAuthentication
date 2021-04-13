fa3eece9a60e27d9b38df4ce5d9d86326afac916
Ramona <jackson.l.ehlert@gmail.com>
2021-04-12 23:16:02 -0600

    W12C User Authentication

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
+
+// This function runs if the axios.request is successful 
+// it sets the API token to the cookie login token and sends a
+//  message that the login was successful the set timeout function is then used to call
+//  a function that redirects to the next page, 1 second after so user can read the message
+
+function loginUserSuccess(res) {
+    let token = res.data.token;
+    Cookies.set("loginToken", token);
+    document.getElementById('successMessage').innerHTML = "<h2>Login was successful</h2>";
+    setTimeout(redirectLogin, 1000);
+
+}
+
+// This function runs if the axios.request is unsuccessful 
+// it posts a message that tells the user they were unsuccessful
+function loginUserFailure(err) {
+    console.log(err);
+    document.getElementById('successMessage').innerHTML = "<h2>Login was unsuccessful</h2>"
+}
+
+
+// This function will be called on click
+// this function uses axios to send and receive the api request
+// We use the POST method as instructed by the API's documentation
+// using the input from  .value and an html form to verify the logins authenticity
 function loginUser(e) {
     document.getElementById('successMessage').innerHTML = "<h2>verifying credentials</h2>"
     axios.request({
@@ -13,25 +39,12 @@ function loginUser(e) {
     }).then(loginUserSuccess).catch(loginUserFailure);
 }
 
-function loginUserSuccess(res) {
-    let token = res.data.token;
-    Cookies.set("loginToken", token);
-    document.getElementById('successMessage').innerHTML = "<h2>Login was successful</h2>"
-    // redirectLogin();
-    setTimeout(redirectLogin, 1000)
-    
-// 2; SHOW USER LOGIN SUCCESS Message -->
-// send user to 2nd page
-}
 
-function loginUserFailure(err) {
-    console.log(err);
-    document.getElementById('successMessage').innerHTML = "<h2>Login was unsuccessful</h2>"
-}
-
+// this function redirects you to page.html
 function redirectLogin() {
     window.location = "/pages/page.html";
 }
 
+// add event listener to the login button
 let loginButton = document.getElementById("loginButton");
 loginButton.addEventListener("click", loginUser)
--- js/page.js
+++ js/page.js
@@ -1,7 +1,10 @@
-token = Cookies.get("loginToken");
+// get the login token from app.js
+let token = Cookies.get("loginToken");
 console.log(token);
 
-
+// if the token is undefined let the user know that no on is logged in 
+// and there is a button that takes you back
+// if someone is logged in, run the color array function
 if(token === undefined) {
 document.getElementById('loggedInMessage').outerHTML = `<p>No one is logged in</p><button onclick="goLogin()">Go Login</button>`;
 document.getElementById('logoutButton').style.display = "none";
@@ -9,6 +12,9 @@ document.getElementById('logoutButton').style.display = "none";
     colorsArray();
 }
 
+// if the axios request is successful this function will run
+// run a loop over the array of colors that we requested, and display them 
+// as well as set their background color style as the hex code from the array
 function colorsArraySuccess(res) {
     let colorArray = res.data.data;
     console.log(colorArray);
@@ -19,9 +25,14 @@ function colorsArraySuccess(res) {
     }
 }
 
+// if the axios request is unsuccessful this function will run
 function colorsArrayFailure(err) {
     console.log(err);
 }
+
+// This function will be called if the user is logged in
+// this function uses axios to send and receive the api request for the color array
+// We use the GET method
 
 function colorsArray(){
     axios.request({
@@ -30,10 +41,14 @@ function colorsArray(){
     }).then(colorsArraySuccess).catch(colorsArrayFailure);
 }
 
+// this function sends you back to the homepage
+// this only displays if you are logged out
 function goLogin() {
     window.location = "/index.html";
 }
 
+// this function runs on click and it removes the token cookie
+// and reloads the page, logging out the user
 function goLogout(e) {
     Cookies.remove("loginToken");
     location.reload();
